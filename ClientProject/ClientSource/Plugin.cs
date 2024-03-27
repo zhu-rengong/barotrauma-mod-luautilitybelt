e3ec1d4b7f7dbceaa6c706aa7c1c9c34843a7d8b
ZRG <2731412072@qq.com>
2024-03-27 20:41:54 +0800

    添加工程文件

---
+++ ClientProject/ClientSource/Plugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Barotrauma;
+using Microsoft.Xna.Framework;
+
+namespace LuaUtilityBelt
+{
+    public partial class Plugin : IAssemblyPlugin
+    {
+
+    }
+}
---
+++ SharedProject/SharedSource/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Barotrauma;
+using Microsoft.Xna.Framework;
+
+[assembly: IgnoresAccessChecksTo("Barotrauma")]
+
+namespace LuaUtilityBelt
+{
+    public partial class Plugin : IAssemblyPlugin
+    {
+        public void Initialize()
+        {
+
+        }
+
+        public void OnLoadCompleted()
+        {
+
+        }
+
+        public void PreInitPatching()
+        {
+            // Not yet supported: Called during the Barotrauma startup phase before vanilla content is loaded.
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
