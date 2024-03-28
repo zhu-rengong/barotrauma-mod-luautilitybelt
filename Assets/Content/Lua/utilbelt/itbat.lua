51b33738798c84a8d1d4301d6d24272f1cf8614f
ZRG <2731412072@qq.com>
2024-03-29 01:37:48 +0800

    优化重构itbat

--- Assets/Content/Lua/utilbelt/itbat.lua
+++ Assets/Content/Lua/utilbelt/itbat.lua
@@ -3,202 +3,218 @@ local utils = require "utilbelt.csharpmodule.Shared.Utils"
 local spedit = require "utilbelt.spedit"
 local moses = require "moses"
 
----@class itembatchblock
----@field _checkparent boolean
+---@class itembatchunitarg : itembatchblockarg, { [integer]: itembatchblockarg }
+---@class itembatchunit : { [integer]: itembatchblock }
+
+---@class itembatchblockarg
 ---@field ref? itembatch
 ---@field identifiers? string|string[]
 ---@field tags? string|string[]
 ---@field excludedidentifiers? string|string[]
 ---@field excludedtags? string|string[]
 ---@field slotindex? integer|integer[]
----@field slottype? Barotrauma.InvSlotType|Barotrauma.InvSlotType[]
+---@field slottype? userdata[]
 ---@field quality? integer|integer[]
 ---@field equipped? boolean
 ---@field equip? boolean
 ---@field drop? boolean
 ---@field remove? boolean
 ---@field properties? sptbl
----@field _spedit? spedit
----@field predicate? fun(item:Barotrauma.Item):boolean
----@field onmatched? fun(item:Barotrauma.Item)
----@field inventory? itembatchblock|itembatchblock[]
+---@field spedit? spedit
+---@field predicate? fun(item:userdata):boolean
+---@field onmatched? fun(item:userdata)
+---@field inventory? itembatchunitarg
+
+---@class itembatchblock
+---@field logWithMark log
+---@field checkParent boolean
+---@field isRef boolean
+---@field ref? itembatch
+---@field identifiers? string[]
+---@field tags? string[]
+---@field excludedIdentifiers? string[]
+---@field excludedTags? string[]
+---@field slotIndexList? integer[]
+---@field slotTypeList? userdata[]
+---@field qualityList? integer[]
+---@field equipped? boolean
+---@field equip? boolean
+---@field drop? boolean
+---@field remove? boolean
+---@field spedit? spedit
+---@field predicate? fun(item:userdata):boolean
+---@field onMatched? fun(item:userdata)
+---@field inventory? itembatch
 
 ---@class itembatchctx
----@field _character? Barotrauma.Character
+---@field character? userdata
 
 ---@class itembatch
----@overload fun(itembatchunit:itembatchblock|itembatchblock[]):itembatch
----@field _invalid boolean
----@field _itembatchunit itembatchblock[]
-local itembatch = {}
+---@field itemBatchUnit itembatchunit
+---@overload fun(itemBatchUnitArg: itembatchunitarg, debugName?: string, parentMark?: string):itembatch
+local m = Class 'itembatch'
+m._ISITEMBATCH = true
 
----@param itembatchunit itembatchblock[]
----@param items Barotrauma.Item[]
----@param context itembatchctx
----@param debugname? string
-local function run(itembatchunit, items, context, debugname)
-    local log = function(text, pattern)
-        if debugname then
-            log(("[DebugName:%s] %s"):format(debugname, text), pattern)
-        else
-            log(text, pattern)
+---@param itemBatchUnitArg itembatchunitarg
+---@param debugName? string
+---@param parentMark? string
+function m:__init(itemBatchUnitArg, debugName, parentMark)
+    local itemBatchUnit = {}
+    self.itemBatchUnit = itemBatchUnit
+
+    local initialIndexType = type(next(itemBatchUnitArg))
+    itemBatchUnitArg = (initialIndexType == "number" or initialIndexType == "nil") and itemBatchUnitArg or { itemBatchUnitArg }
+
+    moses.forEachi(itemBatchUnitArg, function(itemBlockArg, index)
+        if type(itemBlockArg) == "string" then
+            debugName = #itemBlockArg > 0 and itemBlockArg or nil
+            return
         end
-    end
 
-    moses.forEachi(items, function(item)
-        moses.forEachi(itembatchunit, function(itemblock)
-            if type(itemblock) == "string" then
-                debugname = #itemblock > 0 and itemblock or nil
+        local mark = parentMark and ("%s-%i"):format(parentMark, index) or tostring(index)
+        local internalDebugName = debugName
+        local function logWithMark(text, pattern)
+            if internalDebugName then
+                log(("[层级索引:%s] [调试名称:%s] %s"):format(mark, internalDebugName, text), pattern)
+            else
+                log(("[层级索引:%s] %s"):format(mark, text), pattern)
+            end
+        end
+
+        ---@type itembatchblock
+        local itemBlock = { isRef = false }
+
+        local function initItemBlockToBatchUnit()
+            itemBlock.logWithMark = logWithMark
+            table.insert(itemBatchUnit, itemBlock)
+        end
+
+        if itemBlockArg.ref then
+            if not itemBlockArg.ref._ISITEMBATCH then
+                logWithMark("只许引用ItemBatch！", 'e')
                 return
             end
+            itemBlock.isRef = true
+            itemBlock.ref = itemBlockArg.ref
+            initItemBlockToBatchUnit(); return
+        else
+            if itemBlockArg.properties then
+                itemBlock.spedit = spedit(itemBlockArg.properties, logWithMark)
+            end
 
-            if itemblock.ref then
-                itemblock.ref:run({ item })
+            itemBlock.identifiers = itemBlockArg.identifiers and moses.castArray(itemBlockArg.identifiers) or nil
+            itemBlock.tags = itemBlockArg.tags and moses.castArray(itemBlockArg.tags) or nil
+            itemBlock.excludedIdentifiers = itemBlockArg.excludedidentifiers and moses.castArray(itemBlockArg.excludedidentifiers) or nil
+            itemBlock.excludedTags = itemBlockArg.excludedtags and moses.castArray(itemBlockArg.excludedtags) or nil
+            itemBlock.slotIndexList = itemBlockArg.slotindex and moses.castArray(itemBlockArg.slotindex) or nil
+            itemBlock.slotTypeList = itemBlockArg.slottype and moses.castArray(itemBlockArg.slottype) or nil
+            itemBlock.qualityList = itemBlockArg.quality and moses.castArray(itemBlockArg.quality) or nil
+            itemBlock.equipped = itemBlockArg.equipped
+            itemBlock.equip = itemBlockArg.equip
+            itemBlock.drop = itemBlockArg.drop
+            itemBlock.remove = itemBlockArg.remove
+            itemBlock.predicate = itemBlockArg.predicate
+            itemBlock.onMatched = itemBlockArg.onmatched
+            itemBlock.checkParent = itemBlock.slotIndexList or itemBlock.slotTypeList or itemBlock.equipped ~= nil
+
+            if itemBlockArg.inventory then
+                itemBlock.inventory = New "itembatch" (itemBlockArg.inventory, internalDebugName, mark)
+            end
+
+            initItemBlockToBatchUnit(); return
+        end
+    end)
+end
+
+---@param itemBatchUnit itembatchunit
+---@param items userdata[]
+---@param context itembatchctx
+local function run(itemBatchUnit, items, context)
+    moses.forEachi(itemBatchUnit, function(itemBlock)
+        local logWithMark = itemBlock.logWithMark
+        moses.forEachi(items, function(item)
+            if itemBlock.isRef then
+                run(itemBlock.ref.itemBatchUnit, { item }, {
+                    character = context.character
+                })
                 return
             end
 
             local inventory = item.ParentInventory
-            local owner = nil
+            local owner
             local isAtCharacterInventory = false
             if inventory then
                 owner = inventory.Owner
                 if LuaUserData.TypeOf(inventory) == "Barotrauma.CharacterInventory" then
                     isAtCharacterInventory = true
-                    context._character = owner
+                    context.character = owner
                 end
-            elseif itemblock._checkparent then
+            elseif itemBlock.checkParent then
                 return
             end
 
             local identifier = item.Prefab.Identifier.Value
-            if itemblock.excludedidentifiers and moses.include(itemblock.excludedidentifiers, identifier) then return end
-            if itemblock.excludedtags and moses.include(itemblock.excludedtags, function(tag) return item.HasTag(tag) end) then return end
-            if itemblock.identifiers and not moses.include(itemblock.identifiers, identifier) then return end
-            if itemblock.tags and not moses.include(itemblock.tags, function(tag) return item.HasTag(tag) end) then return end
-            if itemblock.quality and not moses.include(itemblock.quality, item.Quality) then return end
+            if itemBlock.excludedIdentifiers and moses.include(itemBlock.excludedIdentifiers, identifier) then return end
+            if itemBlock.excludedTags and moses.include(itemBlock.excludedTags, function(tag) return item.HasTag(tag) end) then return end
+            if itemBlock.identifiers and not moses.include(itemBlock.identifiers, identifier) then return end
+            if itemBlock.tags and not moses.include(itemBlock.tags, function(tag) return item.HasTag(tag) end) then return end
+            if itemBlock.qualityList and not moses.include(itemBlock.qualityList, item.Quality) then return end
 
-            if itemblock.slotindex and not moses.include(itemblock.slotindex, function(slotindex)
-                    return inventory.IsInSlot(item, slotindex)
+            if itemBlock.slotIndexList and not moses.include(itemBlock.slotIndexList, function(slotIndex)
+                    return inventory.IsInSlot(item, slotIndex)
                 end) then
                 return
             end
 
-            if itemblock.slottype and not (isAtCharacterInventory and moses.include(itemblock.slottype, function(slottype)
-                    return inventory.IsInLimbSlot(item, slottype)
+            if itemBlock.slotTypeList and not (isAtCharacterInventory and moses.include(itemBlock.slotTypeList, function(slotType)
+                    return inventory.IsInLimbSlot(item, slotType)
                 end)) then
                 return
             end
 
-            if itemblock.equipped ~= nil then
+            if itemBlock.equipped ~= nil then
                 if isAtCharacterInventory then
-                    if moses.isunique({ itemblock.equipped, owner.HasEquippedItem(item) }) then return end
-                elseif itemblock.equipped then
+                    if moses.isuniq { itemBlock.equipped, owner:HasEquippedItem(item) } then return end
+                elseif itemBlock.equipped then
                     return
                 end
             end
 
-            if itemblock.predicate and not itemblock.predicate(item) then return end
-            if itemblock.onmatched then itemblock.onmatched(item) end
+            if itemBlock.predicate and not itemBlock.predicate(item) then return end
+            if itemBlock.onMatched then itemBlock.onMatched(item) end
 
-            if itemblock.drop then item.Drop() end
-            if itemblock.remove then Entity.Spawner.AddItemToRemoveQueue(item) end
-            if itemblock.equip and context._character then
-                utils.Equip(context._character, item)
+            if itemBlock.drop then item.Drop() end
+            if itemBlock.remove then Entity.Spawner.AddItemToRemoveQueue(item) end
+            if itemBlock.equip and context.character then
+                utils.Equip(context.character, item)
             end
 
-            if itemblock._spedit then
-                itemblock._spedit:apply(item, log)
+            if itemBlock.spedit then
+                itemBlock.spedit:apply(item, logWithMark)
             end
 
-            if itemblock.inventory then
+            if itemBlock.inventory then
                 local itemContainer = utils.GetComponent(item, "ItemContainer")
                 if itemContainer then
-                    run(itemblock.inventory, moses.tabulate(itemContainer.Inventory.AllItemsMod), {
-                        _character = context._character
-                    }, debugname)
+                    run(itemBlock.inventory.itemBatchUnit, moses.tabulate(itemContainer.Inventory.AllItemsMod), {
+                        character = context.character
+                    })
                 else
-                    log(("Cannot batch items in item(%s)'s inventory since it has no inventory!"):format(identifier), 'e')
+                    logWithMark(("无法批处理子物品！原因是没有找到物品'%s'的容器。"):format(tostring(item)), 'e')
                 end
             end
         end)
     end)
 end
 
----@param items Barotrauma.Item[]
----@param debugname? string
-function itembatch:run(items, debugname)
-    run(self._itembatchunit, items, {}, debugname)
+---@param items userdata[]
+function m:run(items)
+    run(self.itemBatchUnit, items, {})
 end
 
----@param inventory Barotrauma.Inventory
----@param debugname? string
-function itembatch:runforinv(inventory, debugname)
-    run(self._itembatchunit, moses.tabulate(inventory.AllItemsMod), {}, debugname)
+---@param inventory userdata
+function m:runforinv(inventory)
+    run(self.itemBatchUnit, moses.tabulate(inventory.AllItemsMod), {})
 end
 
-itembatch.__index = itembatch
-setmetatable(itembatch, {
-    ---@param itembatchunit itembatchblock|itembatchblock[]
-    __call = function(_, itembatchunit)
-        ---@param itbatunit itembatchblock|itembatchblock[]
-        ---@param debugname? string
-        local function construct(itbatunit, debugname)
-            local log = function(text, pattern)
-                if debugname then
-                    log(("[DebugName:%s] %s"):format(debugname, text), pattern)
-                else
-                    log(text, pattern)
-                end
-            end
-            local k1type = type(next(itbatunit))
-            itbatunit = (k1type == "number" or k1type == "nil") and itbatunit or { itbatunit }
-            local _itbatunit = moses.filter(itbatunit, function(itemblock)
-                if type(itemblock) == "string" then
-                    debugname = #itemblock > 0 and itemblock or nil
-                    return true
-                end
-                if itemblock.ref then
-                    if itemblock.ref._invalid then
-                        log(("itemblock is referenced to an invalid itembatch!"), 'e')
-                        return false
-                    end
-                    return true
-                else
-                    if itemblock.properties then
-                        itemblock._spedit = spedit(itemblock.properties, log)
-                    end
-
-                    itemblock.identifiers = itemblock.identifiers and moses.castArray(itemblock.identifiers) or nil
-                    itemblock.tags = itemblock.tags and moses.castArray(itemblock.tags) or nil
-                    itemblock.excludedidentifiers = itemblock.excludedidentifiers and moses.castArray(itemblock.excludedidentifiers) or nil
-                    itemblock.excludedtags = itemblock.excludedtags and moses.castArray(itemblock.excludedtags) or nil
-                    itemblock.slotindex = itemblock.slotindex and moses.castArray(itemblock.slotindex) or nil
-                    itemblock.slottype = itemblock.slottype and moses.castArray(itemblock.slottype) or nil
-                    itemblock.quality = itemblock.quality and moses.castArray(itemblock.quality) or nil
-
-                    itemblock._checkparent = itemblock.slotindex or itemblock.slottype or itemblock.equipped ~= nil
-
-                    if itemblock.inventory then
-                        itemblock.inventory = type(next(itemblock.inventory)) == "number"
-                            and itemblock.inventory or { itemblock.inventory }
-                        itemblock.inventory = construct(itemblock.inventory, debugname)
-                    end
-                    return true
-                end
-            end)
-            return _itbatunit
-        end
-
-        ---@type itembuilder
-        local inst = setmetatable({ _itembatchunit = construct(itembatchunit) }, itembatch)
-
-        if #inst._itembatchunit == 0 then
-            inst._invalid = true
-            log("itembatchunit is invalid!", 'e')
-        end
-
-        return inst
-    end,
-})
-
-return itembatch
+return m
